c75722fa7cc8c83c2c4b507d642a5b73428f96ad
Vlamonster <jellemartijndijkstra@gmail.com>
2022-12-21 00:47:00 +0100

    Solution for day 13b.

---
+++ d13b/src/main.rs
@@ -0,0 +1,76 @@
+use crate::Node::{Array, Number};
+use itertools::Itertools;
+use std::cmp::Ordering;
+use std::str::Chars;
+
+#[derive(Debug, PartialEq, Eq)]
+enum Node {
+    Array(Vec<Node>),
+    Number(usize),
+}
+
+impl PartialOrd<Self> for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Number(left), Number(right)) => left.cmp(right),
+            (Array(left), Array(right)) => left.iter().cmp(right),
+            (Number(left), Array(_)) => Array(vec![Number(*left)]).cmp(other),
+            (Array(_), Number(right)) => self.cmp(&Array(vec![Number(*right)])),
+        }
+    }
+}
+
+fn parse(input: &str) -> Node {
+    let mut to_parse = input.chars();
+    to_parse.next();
+    parse_recursively(&mut to_parse)
+}
+
+fn parse_recursively(to_parse: &mut Chars) -> Node {
+    let mut result = Vec::new();
+    let mut number = None;
+    while let Some(char) = to_parse.next() {
+        match char {
+            '[' => result.push(parse_recursively(to_parse)),
+            ',' => {
+                if let Some(value) = number.take() {
+                    result.push(Number(value as usize));
+                }
+            }
+            '0'..='9' => match number {
+                None => number = Some((char as u8 - b'0') as isize),
+                Some(value) => number = Some(value * 10 + (char as u8 - b'0') as isize),
+            },
+            ']' => {
+                if let Some(value) = number {
+                    result.push(Number(value as usize))
+                }
+                return Array(result);
+            }
+            _ => unreachable!(),
+        }
+    }
+    Array(result)
+}
+
+fn main() {
+    print!(
+        "{:?}",
+        include_str!("input.txt")
+            .lines()
+            .filter(|line| !line.is_empty())
+            .chain(["[[2]]", "[[6]]"])
+            .map(parse)
+            .sorted()
+            .enumerate()
+            .filter(|(_, y)| *y == parse("[[2]]") || *y == parse("[[6]]"))
+            .map(|(x, _)| x + 1)
+            .product::<usize>()
+    );
+}
