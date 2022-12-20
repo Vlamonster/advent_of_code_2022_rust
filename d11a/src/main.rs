80f50e4f5f90627986a8a0ae35df8d0d51ff3f1d
Vlamonster <jellemartijndijkstra@gmail.com>
2022-12-20 18:12:54 +0100

    Cleanup solution 11.

--- d11a/src/main.rs
+++ d11a/src/main.rs
@@ -1,9 +1,8 @@
-use crate::Operation::{Add, Double, Mul, Square};
-use regex::Regex;
+use crate::Operation::{Add, Mul, Square};
+use itertools::Itertools;
 
 enum Operation {
     Square,
-    Double,
     Mul(usize),
     Add(usize),
 }
@@ -17,106 +16,103 @@ struct Monkey {
     inspections: usize,
 }
 
-impl Monkey {
-    pub fn from_information(information: &str) -> Self {
-        let integer_re = Regex::new(r"\d+").unwrap();
-        let equation_re = Regex::new(r"old|\d+|[*+]").unwrap();
+fn parse_monkeys() -> Vec<Monkey> {
+    let mut monkeys = Vec::new();
+    for monkey_data in include_str!("input.txt").split("\n\n") {
+        let mut monkey_data = monkey_data.lines().skip(1);
 
-        let mut lines = information.lines().skip(1);
-        let items = integer_re
-            .find_iter(lines.next().unwrap())
-            .map(|re_match| re_match.as_str().parse::<usize>().unwrap())
-            .collect::<Vec<_>>();
-        let mut operation_matches = equation_re.find_iter(lines.next().unwrap()).skip(1);
-        let operation = match operation_matches.next().unwrap().as_str() {
-            "+" => match operation_matches.next().unwrap().as_str() {
-                "old" => Double,
-                operand => Add(operand.parse::<usize>().unwrap()),
-            },
-            "*" => match operation_matches.next().unwrap().as_str() {
-                "old" => Square,
-                operand => Mul(operand.parse::<usize>().unwrap()),
-            },
+        let items = monkey_data
+            .next()
+            .unwrap()
+            .strip_prefix("  Starting items: ")
+            .unwrap()
+            .split(", ")
+            .map(|item| item.parse().unwrap())
+            .collect_vec();
+
+        let operation = match monkey_data
+            .next()
+            .unwrap()
+            .strip_prefix("  Operation: new = old ")
+            .unwrap()
+            .split_once(' ')
+            .unwrap()
+        {
+            ("*", "old") => Square,
+            ("*", operand) => Mul(operand.parse().unwrap()),
+            ("+", operand) => Add(operand.parse().unwrap()),
             _ => unreachable!(),
         };
-        let test = integer_re
-            .find(lines.next().unwrap())
+
+        let test = monkey_data
+            .next()
             .unwrap()
-            .as_str()
-            .parse::<usize>()
-            .unwrap();
-        let test_true = integer_re
-            .find(lines.next().unwrap())
+            .strip_prefix("  Test: divisible by ")
             .unwrap()
-            .as_str()
-            .parse::<usize>()
+            .parse()
             .unwrap();
-        let test_false = integer_re
-            .find(lines.next().unwrap())
+
+        let test_true = monkey_data
+            .next()
             .unwrap()
-            .as_str()
-            .parse::<usize>()
+            .strip_prefix("    If true: throw to monkey ")
+            .unwrap()
+            .parse()
             .unwrap();
-        Monkey {
+
+        let test_false = monkey_data
+            .next()
+            .unwrap()
+            .strip_prefix("    If false: throw to monkey ")
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        monkeys.push(Monkey {
             items,
             operation,
             test,
             test_true,
             test_false,
             inspections: 0,
-        }
+        })
     }
-
-    pub fn inspect_items(&mut self) {
-        for item in &mut self.items {
-            self.inspections += 1;
-            *item = match self.operation {
-                Square => *item * *item,
-                Double => *item + *item,
-                Mul(operand) => *item * operand,
-                Add(operand) => *item + operand,
-            } / 3;
-        }
-    }
-
-    pub fn test_worry(&self, item: &usize) -> usize {
-        if item % self.test == 0 {
-            self.test_true
-        } else {
-            self.test_false
-        }
-    }
-}
-
-fn do_round(monkeys: &mut Vec<Monkey>) {
-    for monkey_id in 0..monkeys.len() {
-        monkeys[monkey_id].inspect_items();
-        let items = monkeys[monkey_id].items.clone();
-        for item in &items {
-            let new_monkey = monkeys[monkey_id].test_worry(item);
-            monkeys[new_monkey].items.push(*item);
-        }
-        monkeys[monkey_id].items.clear();
-    }
+    monkeys
 }
 
 fn main() {
-    let mut monkeys = Vec::new();
-    for information in include_str!("input.txt").split("\n\n") {
-        monkeys.push(Monkey::from_information(information));
-    }
+    let mut monkeys = parse_monkeys();
 
     for _ in 0..20 {
-        do_round(&mut monkeys);
+        for monkey_id in 0..monkeys.len() {
+            while let Some(mut item) = monkeys[monkey_id].items.pop() {
+                monkeys[monkey_id].inspections += 1;
+
+                item = match monkeys[monkey_id].operation {
+                    Square => item * item,
+                    Mul(operand) => item * operand,
+                    Add(operand) => item + operand,
+                } / 3;
+
+                let target_monkey = if item % monkeys[monkey_id].test == 0 {
+                    monkeys[monkey_id].test_true
+                } else {
+                    monkeys[monkey_id].test_false
+                };
+
+                monkeys[target_monkey].items.push(item);
+            }
+        }
     }
 
-    let mut inspections = monkeys
-        .iter()
-        .map(|monkey| monkey.inspections)
-        .collect::<Vec<_>>();
-    inspections.sort();
     print!(
-        "{}",
-        inspections.pop().unwrap() * inspections.pop().unwrap()
+        "{:?}",
+        monkeys
+            .iter()
+            .map(|monkey| monkey.inspections)
+            .sorted()
+            .rev()
+            .take(2)
+            .product::<usize>()
     );
 }
--- d11b/src/main.rs
+++ d11b/src/main.rs
@@ -1,9 +1,8 @@
-use crate::Operation::{Add, Double, Mul, Square};
-use regex::Regex;
+use crate::Operation::{Add, Mul, Square};
+use itertools::Itertools;
 
 enum Operation {
     Square,
-    Double,
     Mul(usize),
     Add(usize),
 }
@@ -17,109 +16,104 @@ struct Monkey {
     inspections: usize,
 }
 
-impl Monkey {
-    pub fn from_information(information: &str) -> Self {
-        let integer_re = Regex::new(r"\d+").unwrap();
-        let equation_re = Regex::new(r"old|\d+|[*+]").unwrap();
+fn parse_monkeys() -> Vec<Monkey> {
+    let mut monkeys = Vec::new();
+    for monkey_data in include_str!("input.txt").split("\n\n") {
+        let mut monkey_data = monkey_data.lines().skip(1);
 
-        let mut lines = information.lines().skip(1);
-        let items = integer_re
-            .find_iter(lines.next().unwrap())
-            .map(|re_match| re_match.as_str().parse::<usize>().unwrap())
-            .collect::<Vec<_>>();
-        let mut operation_matches = equation_re.find_iter(lines.next().unwrap()).skip(1);
-        let operation = match operation_matches.next().unwrap().as_str() {
-            "+" => match operation_matches.next().unwrap().as_str() {
-                "old" => Double,
-                operand => Add(operand.parse::<usize>().unwrap()),
-            },
-            "*" => match operation_matches.next().unwrap().as_str() {
-                "old" => Square,
-                operand => Mul(operand.parse::<usize>().unwrap()),
-            },
+        let items = monkey_data
+            .next()
+            .unwrap()
+            .strip_prefix("  Starting items: ")
+            .unwrap()
+            .split(", ")
+            .map(|item| item.parse().unwrap())
+            .collect_vec();
+
+        let operation = match monkey_data
+            .next()
+            .unwrap()
+            .strip_prefix("  Operation: new = old ")
+            .unwrap()
+            .split_once(' ')
+            .unwrap()
+        {
+            ("*", "old") => Square,
+            ("*", operand) => Mul(operand.parse().unwrap()),
+            ("+", operand) => Add(operand.parse().unwrap()),
             _ => unreachable!(),
         };
-        let test = integer_re
-            .find(lines.next().unwrap())
+
+        let test = monkey_data
+            .next()
             .unwrap()
-            .as_str()
-            .parse::<usize>()
-            .unwrap();
-        let test_true = integer_re
-            .find(lines.next().unwrap())
+            .strip_prefix("  Test: divisible by ")
             .unwrap()
-            .as_str()
-            .parse::<usize>()
+            .parse()
             .unwrap();
-        let test_false = integer_re
-            .find(lines.next().unwrap())
+
+        let test_true = monkey_data
+            .next()
             .unwrap()
-            .as_str()
-            .parse::<usize>()
+            .strip_prefix("    If true: throw to monkey ")
+            .unwrap()
+            .parse()
             .unwrap();
-        Monkey {
+
+        let test_false = monkey_data
+            .next()
+            .unwrap()
+            .strip_prefix("    If false: throw to monkey ")
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        monkeys.push(Monkey {
             items,
             operation,
             test,
             test_true,
             test_false,
             inspections: 0,
-        }
+        })
     }
-
-    pub fn inspect_items(&mut self, lcm: usize) {
-        for item in &mut self.items {
-            self.inspections += 1;
-            *item = match self.operation {
-                Square => *item * *item,
-                Double => *item + *item,
-                Mul(operand) => *item * operand,
-                Add(operand) => *item + operand,
-            } % lcm;
-        }
-    }
-
-    pub fn test_worry(&self, item: &usize) -> usize {
-        if item % self.test == 0 {
-            self.test_true
-        } else {
-            self.test_false
-        }
-    }
-}
-
-fn do_round(monkeys: &mut Vec<Monkey>, lcm: usize) {
-    for monkey_id in 0..monkeys.len() {
-        monkeys[monkey_id].inspect_items(lcm);
-        let items = monkeys[monkey_id].items.clone();
-        for item in &items {
-            let new_monkey = monkeys[monkey_id].test_worry(item);
-            monkeys[new_monkey].items.push(*item);
-        }
-        monkeys[monkey_id].items.clear();
-    }
+    monkeys
 }
 
 fn main() {
-    let mut monkeys = Vec::new();
-    for information in include_str!("input.txt").split("\n\n") {
-        monkeys.push(Monkey::from_information(information));
-    }
-
-    // technically not lcm, but it works
-    let lcm = monkeys.iter().map(|monkey| monkey.test).product();
+    let mut monkeys = parse_monkeys();
+    let lcm = monkeys.iter().map(|monkey| monkey.test).product::<usize>(); // technically not lcm, but it works
 
     for _ in 0..10000 {
-        do_round(&mut monkeys, lcm);
+        for monkey_id in 0..monkeys.len() {
+            while let Some(mut item) = monkeys[monkey_id].items.pop() {
+                monkeys[monkey_id].inspections += 1;
+
+                item = match monkeys[monkey_id].operation {
+                    Square => item * item,
+                    Mul(operand) => item * operand,
+                    Add(operand) => item + operand,
+                } % lcm;
+
+                let target_monkey = if item % monkeys[monkey_id].test == 0 {
+                    monkeys[monkey_id].test_true
+                } else {
+                    monkeys[monkey_id].test_false
+                };
+
+                monkeys[target_monkey].items.push(item);
+            }
+        }
     }
 
-    let mut inspections = monkeys
-        .iter()
-        .map(|monkey| monkey.inspections)
-        .collect::<Vec<_>>();
-    inspections.sort();
     print!(
-        "{}",
-        inspections.pop().unwrap() * inspections.pop().unwrap()
+        "{:?}",
+        monkeys
+            .iter()
+            .map(|monkey| monkey.inspections)
+            .sorted()
+            .rev()
+            .take(2)
+            .product::<usize>()
     );
 }
