1b469b3c5c19692643419102325738fb2dd24d0f
Vlamonster <jellemartijndijkstra@gmail.com>
2022-12-24 09:18:44 +0100

    Cleanup solution for day 8a.

--- d08a/src/main.rs
+++ d08a/src/main.rs
@@ -1,70 +1,24 @@
-use std::cmp::max;
+use itertools::Itertools;
 
 fn main() {
     let forest = include_str!("input.txt")
         .lines()
-        .map(|x| x.chars().map(|y| y as u8 - b'0').collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+        .map(|x| x.chars().map(|y| y as isize - '0' as isize).collect_vec())
+        .collect_vec();
 
-    let forest_width = forest[0].len();
-    let forest_height = forest.len();
+    let width = forest[0].len();
+    let height = forest.len();
 
-    let mut top_view: Vec<Vec<u8>> = vec![vec![0; forest_width]; forest_height];
-    let mut bottom_view: Vec<Vec<u8>> = vec![vec![0; forest_width]; forest_height];
-    let mut left_view: Vec<Vec<u8>> = vec![vec![0; forest_width]; forest_height];
-    let mut right_view: Vec<Vec<u8>> = vec![vec![0; forest_width]; forest_height];
-
-    for y in 0..forest_height {
-        for x in 0..forest_width {
-            if y == 0 {
-                top_view[y][x] = forest[y][x];
-            } else {
-                top_view[y][x] = max(forest[y][x], top_view[y - 1][x]);
-            }
-        }
-    }
-    for y in (0..forest_height).rev() {
-        for x in 0..forest_width {
-            if y == forest_height - 1 {
-                bottom_view[y][x] = forest[y][x];
-            } else {
-                bottom_view[y][x] = max(forest[y][x], bottom_view[y + 1][x]);
-            }
-        }
-    }
-    for x in 0..forest_width {
-        for y in 0..forest_height {
-            if x == 0 {
-                left_view[y][x] = forest[y][x];
-            } else {
-                left_view[y][x] = max(forest[y][x], left_view[y][x - 1]);
-            }
-        }
-    }
-    for x in (0..forest_width).rev() {
-        for y in 0..forest_height {
-            if x == forest_width - 1 {
-                right_view[y][x] = forest[y][x];
-            } else {
-                right_view[y][x] = max(forest[y][x], right_view[y][x + 1]);
-            }
-        }
-    }
-
-    let mut visible_trees = 0;
-    for y in 0..forest_height {
-        for x in 0..forest_width {
-            if y == 0 || y == forest_height - 1 || x == 0 || x == forest_width - 1 {
-                visible_trees += 1;
-            } else if forest[y][x] > top_view[y - 1][x]
-                || forest[y][x] > bottom_view[y + 1][x]
-                || forest[y][x] > left_view[y][x - 1]
-                || forest[y][x] > right_view[y][x + 1]
-            {
-                visible_trees += 1;
-            }
-        }
-    }
-
-    print!("{}", visible_trees);
+    print!(
+        "{}",
+        (0..width)
+            .cartesian_product(0..height)
+            .filter(|&(x, y)| {
+                forest[y][x] > (0..x).map(|nx| forest[y][nx]).max().unwrap_or(-1)
+                    || forest[y][x] > (x + 1..width).map(|nx| forest[y][nx]).max().unwrap_or(-1)
+                    || forest[y][x] > (0..y).map(|ny| forest[ny][x]).max().unwrap_or(-1)
+                    || forest[y][x] > (y + 1..height).map(|ny| forest[ny][x]).max().unwrap_or(-1)
+            })
+            .count()
+    );
 }
