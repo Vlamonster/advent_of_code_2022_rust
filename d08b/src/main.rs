f14aaa1a99ac09560cf048dc90b81528dd875015
Vlamonster <jellemartijndijkstra@gmail.com>
2022-12-24 09:56:47 +0100

    Cleanup solution for day 8b.

--- d08b/src/main.rs
+++ d08b/src/main.rs
@@ -1,52 +1,39 @@
+use itertools::Itertools;
+
 fn main() {
     let forest = include_str!("input.txt")
         .lines()
-        .map(|x| x.chars().map(|y| y as u8 - b'0').collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+        .map(|x| x.chars().map(|y| y as isize - b'0' as isize).collect_vec())
+        .collect_vec();
 
-    let forest_width = forest[0].len();
-    let forest_height = forest.len();
+    let width = forest[0].len();
+    let height = forest.len();
 
-    let mut best_score = 0;
-
-    for y in 0..forest_height {
-        for x in 0..forest_width {
-            let mut top_score = 0;
-            let mut bottom_score = 0;
-            let mut left_score = 0;
-            let mut right_score = 0;
-
-            for i in (0..y).rev() {
-                top_score += 1;
-                if forest[y][x] <= forest[i][x] {
-                    break;
-                }
-            }
-            for i in y + 1..forest_height {
-                bottom_score += 1;
-                if forest[y][x] <= forest[i][x] {
-                    break;
-                }
-            }
-            for i in (0..x).rev() {
-                left_score += 1;
-                if forest[y][x] <= forest[y][i] {
-                    break;
-                }
-            }
-            for i in x + 1..forest_width {
-                right_score += 1;
-                if forest[y][x] <= forest[y][i] {
-                    break;
-                }
-            }
-
-            let score = top_score * bottom_score * left_score * right_score;
-            if score > best_score {
-                best_score = score;
-            }
-        }
-    }
-
-    print! {"{}", best_score};
+    print!(
+        "{}",
+        (0..width)
+            .cartesian_product(0..height)
+            .map(
+                |(x, y)| (0..x)
+                    .rev()
+                    .position(|nx| forest[y][nx] >= forest[y][x])
+                    .unwrap_or_else(|| x.wrapping_sub(1))
+                    .wrapping_add(1)
+                    * (x + 1..width)
+                        .position(|nx| forest[y][nx] >= forest[y][x])
+                        .unwrap_or_else(|| (width - x - 1).wrapping_sub(1))
+                        .wrapping_add(1)
+                    * (0..y)
+                        .rev()
+                        .position(|ny| forest[ny][x] >= forest[y][x])
+                        .unwrap_or_else(|| y.wrapping_sub(1))
+                        .wrapping_add(1)
+                    * (y + 1..height)
+                        .position(|ny| forest[ny][x] >= forest[y][x])
+                        .unwrap_or_else(|| (height - y - 1).wrapping_sub(1))
+                        .wrapping_add(1) //}
+            )
+            .max()
+            .unwrap()
+    );
 }
